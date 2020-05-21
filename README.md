8e608a7028407416aa533bad276fcb893d75a415
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2020-05-21 14:22:29 +0300

    Stage 3: Matrix by matrix multiplication
    add method `matrix_mul` to class `Matrix`
    
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 
 # Project: [Numeric Matrix Processor](https://hyperskill.org/projects/96)
 
-## Stage 1: Objectives
+## Stage 1: Addition
+
+### Objectives
 
 In this stage, you should write a program that:
  1. Reads matrix **A** from the input.
@@ -17,7 +19,26 @@ one row of the matrix.
 Output the result in the same way but don't print the dimensions of the 
 matrix.
 
-## Stage 2: Objectives
+
+## Stage 2: Multiplication by number
+
+### Objectives
 
 Write a program that reads a matrix and a constant and outputs 
 the result of their multiplication.
+
+
+## Stage 3: Matrix by matrix multiplication 
+
+ - [x] [3Blue1Brown: Essence of linear algebra (15 chapters)][Essence of linear algebra]
+
+### Objectives
+
+Write a program that does the following:
+ 1. Prints a menu consisting of 4 options and reads the user's choice.
+ 2. Reads all data (matrices, constants) needed to perform the chosen 
+    operation.
+ 3. Calculates the result and outputs it.
+ 4. Repeats all these steps until the `"Exit"` option is chosen.
+
+[Essence of linear algebra]: https://www.youtube.com/watch?v=fNk_zzaMoSs&list=PLZHQObOWTQDPD3MizzM2xVFitgF8hE_ab
--- processor.py
+++ processor.py
@@ -1,6 +1,11 @@
-from typing import Any, List, Optional, Tuple, Union
+from numbers import Real
+from operator import add, mul
+from sys import exit
+from typing import Any, Callable, Dict, List, NoReturn, Optional, Tuple, Union
 
-MatrixElements = List[int]
+Element = float
+MatrixElements = List[Element]
+MatrixParameters = Tuple[int, int, MatrixElements]
 
 __all__ = ['Matrix']
 
@@ -14,9 +19,10 @@ class Matrix:
             __columns: Optional[int] = None,
             *,
             elements: Optional[MatrixElements] = None,
+            alias: Optional[str] = '',
     ) -> None:
         if not (__rows and __columns and elements):
-            __rows, __columns, elements = self.read_matrix_params_from_input()
+            __rows, __columns, elements = self.read_matrix_parameters_from_input(alias)
         if len(elements) != (__rows * __columns):
             raise ValueError
         self.rows: int = __rows
@@ -35,46 +41,99 @@ class Matrix:
 
     def __add__(self, other: Any) -> Union['Matrix', None]:
         if not isinstance(other, self.__class__):
-            raise NotImplemented
+            raise NotImplementedError
         if self.rows != other.rows or self.columns != other.columns:
-            # this is requirement of task
-            print("ERROR")
-            return None
-        elements: MatrixElements = list(map(sum, zip(self.elements, other.elements)))
+            raise ValueError
+        elements: MatrixElements
+        elements = list(add(*pair) for pair in zip(self.elements, other.elements))
         return Matrix(self.rows, self.columns, elements=elements)
 
     def __mul__(self, other: Any) -> 'Matrix':
-        if not isinstance(other, int):
-            raise NotImplemented
-        elements: MatrixElements = list(map(lambda x: x * other, self.elements))
-        return Matrix(self.rows, self.columns, elements=elements)
+        if isinstance(other, Real):
+            elements: MatrixElements = list(e * other for e in self.elements)
+            return Matrix(self.rows, self.columns, elements=elements)
+        else:
+            raise NotImplementedError
+
+    def matrix_mul(self, other: 'Matrix') -> Union['Matrix', NoReturn]:
+        if not isinstance(other, type(self)):
+            raise TypeError
+        if self.columns != other.rows:
+            raise ValueError
+        elements: MatrixElements = list()
+        self_row: MatrixElements
+        other_column: MatrixElements
+        for r in range(self.rows):
+            first_element: int = r * self.columns
+            self_row = self.elements[first_element: first_element + self.columns:]
+            for c in range(other.columns):
+                other_column = other.elements[c:: other.columns]
+                elements.append(sum(mul(*pair) for pair in zip(self_row, other_column)))
+        return Matrix(self.rows, other.columns, elements=elements)
 
     @staticmethod
-    def read_matrix_params_from_input() -> Tuple[int, int, MatrixElements]:
+    def read_matrix_parameters_from_input(
+            alias: Optional[str] = '',
+    ) -> MatrixParameters:
+        print(
+            "Enter size of", f" {alias} " if alias else " ", "matrix:", sep='', end=' '
+        )
         rows, columns = map(int, input().split(' ', 1))
         elements: MatrixElements = list()
+        row: MatrixElements
+        print("Enter", f" {alias} " if alias else " ", "matrix:", sep='')
         for _ in range(rows):
-            row: List[int] = list(map(int, input().split()))
+            row = [float(x) if '.' in x else int(x) for x in input().split()]
             if len(row) != columns:
                 raise ValueError
             elements.extend(row)
         return rows, columns, elements
 
 
+def print_result(matrix: Matrix) -> None:
+    print("The result is:", matrix, "", sep='\n')
+
+
 def addition() -> None:
     # Stage #1: Addition
-    matrix_a: Matrix = Matrix()
-    matrix_b: Matrix = Matrix()
-    matrix = matrix_a + matrix_b
+    matrix_a: Matrix = Matrix(alias='first')
+    matrix_b: Matrix = Matrix(alias='second')
+    matrix: Optional[Matrix] = matrix_a + matrix_b
     if matrix:
-        print(matrix)
+        print_result(matrix)
 
 
 def multiplication_by_number() -> None:
-    matrix = Matrix()
-    constant = int(input())
-    print(matrix * constant)
+    # Stage 2: Multiplication by number
+    matrix: Matrix = Matrix()
+    constant: str = input("Enter constant: ")
+    number: float = float(constant) if '.' in constant else int(constant)
+    print_result(matrix * number)
+
+
+def matrix_by_matrix_multiplication() -> None:
+    # Stage 3: Matrix by matrix multiplication
+    matrix_a: Matrix = Matrix(alias='first')
+    matrix_b: Matrix = Matrix(alias='second')
+    print_result(matrix_a.matrix_mul(matrix_b))
+
+
+def main() -> NoReturn:
+    menu_options: Dict[str, Tuple[str, Callable[..., Any]]] = {
+        '1': ("Add matrices", addition),
+        '2': ("Multiply matrix by a constant", multiplication_by_number),
+        '3': ("Multiply matrices", matrix_by_matrix_multiplication),
+        '0': ("Exit", exit),
+    }
+    while True:
+        print(*(f"{num}. {value[0]}" for num, value in menu_options.items()), sep='\n')
+        choice = input("Your choice: ")
+        if choice in menu_options:
+            try:
+                menu_options[choice][1]()
+            except (ValueError, NotImplementedError, TypeError):
+                print("The operation cannot be performed.\n")
 
 
 if __name__ == '__main__':
-    multiplication_by_number()
+    main()
